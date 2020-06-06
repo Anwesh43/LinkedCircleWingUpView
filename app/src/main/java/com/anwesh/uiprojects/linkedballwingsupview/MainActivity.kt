b42bed3c8b581c6ed3538967a1b15c2868745b3e
Anwesh43 <anweshthecool0@gmail.com>
2020-06-06 09:04:30 +0530

    adding project setup

---
+++ app/src/main/java/com/anwesh/uiprojects/linkedballwingsupview/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.anwesh.uiprojects.linkedballwingsupview
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by anweshmishra on 06/06/20.
+ */
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(instance : Bundle?) {
+        super.onCreate(instance)
+    }
+}
---
+++ ballwingsupview/src/main/java/com/anwesh/uiprojects/ballwingsupview/BallWingsUpView.kt
@@ -0,0 +1,5 @@
+package com.anwesh.uiprojects.ballwingsupview
+
+/**
+ * Created by anweshmishra on 06/06/20.
+ */
