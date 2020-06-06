be97eb199fb88347dae4694e504b79bee321c399
Anwesh43 <anweshthecool0@gmail.com>
2020-06-06 10:35:22 +0530

    adding BWUNode class

--- ballwingsupview/src/main/java/com/anwesh/uiprojects/ballwingsupview/BallWingsUpView.kt
+++ ballwingsupview/src/main/java/com/anwesh/uiprojects/ballwingsupview/BallWingsUpView.kt
@@ -127,4 +127,45 @@ class BallWingsUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BWUNode(var i : Int, val state : State = State()) {
+
+        private var next : BWUNode? = null
+        private var prev : BWUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BWUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBWUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BWUNode {
+            var curr : BWUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
