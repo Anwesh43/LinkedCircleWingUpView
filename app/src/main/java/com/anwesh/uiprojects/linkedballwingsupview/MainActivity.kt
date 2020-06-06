8a2a7de53cea308c3a7cdbb647842f631b0cdc38
Anwesh43 <anweshthecool0@gmail.com>
2020-06-06 10:56:31 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedballwingsupview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedballwingsupview/MainActivity.kt
@@ -2,6 +2,8 @@ package com.anwesh.uiprojects.linkedballwingsupview
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
+import com.anwesh.uiprojects.ballwingsupview.BallWingsUpView
 
 /**
  * Created by anweshmishra on 06/06/20.
@@ -11,5 +13,12 @@ class MainActivity: AppCompatActivity() {
 
     override fun onCreate(instance : Bundle?) {
         super.onCreate(instance)
+        BallWingsUpView.create(this)
+        fullScreen()
+    }
+
+    fun MainActivity.fullScreen() {
+        supportActionBar?.hide()
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
